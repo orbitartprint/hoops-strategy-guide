da7d082e6c25c803a4a34e9799e00fec826aa54c
orbitartprint <orbitartprint@gmail.com>
2025-04-27 00:06:00 +0200

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -10,12 +10,12 @@ const Index = () => {
         <img
           src="/lovable-uploads/0ec61829-fdfd-4d0d-afaa-8cca6fa651d1.png"
           alt="TSV Logo"
-          className="w-64 h-auto mb-4"
+          className="w-24 h-auto mb-8"
         />
         <img
           src="/lovable-uploads/39357a08-4c38-4fa9-8536-a4bdcdaa5343.png"
           alt="Basketball Logo"
-          className="w-24 h-auto mb-8"
+          className="w-64 h-auto mb-4"
         />
         <h1 className="text-4xl md:text-5xl font-bold text-center mb-8">
           Unsere Spielprinzipien
